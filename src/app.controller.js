577d40c59b95f0b5ae58e01c08a794b9859a083f
Ahmed <ahmedelhawaryae129@gmail.com>
2025-07-27 02:44:50 +0300

    updates

--- src/app.controller.js
+++ src/app.controller.js
@@ -4,10 +4,24 @@ import messageRouter from "./modules/messages/message.controller.js";
 import userRouter from "./modules/users/user.controller.js";
 import { globalErrorHandler } from "./utils/error/index.js";
 
+// limiter
+const limiter = rateLimit({
+    limit : 5,
+    windowMs : 60 * 1000,
+    message : {Error : "Try Again Later"},
+    statusCode : 429,
+    handler : (req, res, next) => {
+        return next(new AppError("Try Again Later", 429))
+    }
+})
+
 const bootstrap = (app, express) => {
 
     // use cors middleware
     app.use(cors());
+
+    // limiter
+    app.use(limiter)
 
     // parse incoming data
     app.use(express.json());
